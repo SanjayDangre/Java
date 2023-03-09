d24673074899a8b8fa3b4ed13b62c659a02466a0
SanjayDangre <127373319+SanjayDangre@users.noreply.github.com>
2023-03-09 14:17:47 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,13 @@
+class Datatype{
+    public static void main(String[] args){
+        byte b=32;
+        short s=1508;
+        int i=956182604;
+        // 9 digit
+        long I=967183104;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(I);
+        }
+}
