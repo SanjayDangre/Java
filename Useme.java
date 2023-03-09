a9889d819a3dc98d1c94bb387467d8a572897c75
SanjayDangre <127373319+SanjayDangre@users.noreply.github.com>
2023-03-09 14:13:06 +0530

    Add files via upload

---
+++ Useme.java
@@ -0,0 +1,10 @@
+class Test{
+    public static void show(){
+        System.out.println("Hi");
+        }
+        }
+class Useme{
+    public static void main(String args[]){
+        Test.show();
+        }
+        }
