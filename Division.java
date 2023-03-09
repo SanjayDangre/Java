8cdbe2bded6b20bc948828abec997f46b797012c
SanjayDangre <127373319+SanjayDangre@users.noreply.github.com>
2023-03-09 10:19:46 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=36;
+		int b=4;
+		int res=a/b;
+		System.out.print("Result is "+res);		
+	}
+}
---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		int res=a*b;
+		System.out.print("Result is "+res);		
+	}
+}
