f190496f5d06266bd4721fbcbc5d4b7129e42082
SanjayDangre <127373319+SanjayDangre@users.noreply.github.com>
2023-03-10 10:17:16 +0530

    Add files via upload

---
+++ Implicit.java
@@ -0,0 +1,12 @@
+class Test{
+    public static void main(String[] args){
+        int i = 200;
+        //automatic type conversion
+        long I = i;
+        //automatic type conversion
+        float f = I;
+        System.out.println("Int value "+i); 
+        System.out.println("Long value "+I);
+         System.out.println("Float value "+f);
+    }
+}
