a898a755f354a08906e76c4a9bcb7baaedbd0a5a
SanjayDangre <127373319+SanjayDangre@users.noreply.github.com>
2023-03-11 13:29:36 +0530

    Add files via upload

---
+++ EX11.java
@@ -0,0 +1,9 @@
+class EX11{
+    public static void main(String []args){
+        float f=0.7f;
+        if(f==0.7)
+        System.out.print("hello");
+        else
+        System.out.print("hi");
+    }
+}
